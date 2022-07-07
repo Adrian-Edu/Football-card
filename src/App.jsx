1d0dc821de352a9f35b8fa0422b26565d24ac2da
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2022-07-07 09:32:29 +0300

    Gata html si css

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ function App() {
   return (
     <div className="page-container">
 
+      <div className='up-container'>
+
       <div className='container'>
 
         <h1>Football Teams</h1>
@@ -37,7 +39,47 @@ function App() {
 
         </div>
 
-       
+        
+
+      </div>
+
+      <div className='right-container'>
+          
+          <div className='right-container-first'> 
+          
+          <img src={ require('./real.png') } />
+          <div className='right-container-first-h-p'>
+          <h4>Real Madrid</h4>
+          <p>Votes</p>
+          </div>
+          
+          
+          </div>
+
+      
+          <div className='right-container-second'> 
+          
+            <img src={ require('./real.png') } />
+            <div className='right-container-first-h-p-second'>
+            <h4>Real Madrid</h4>
+            <p>Votes</p>
+
+          </div>
+          
+          
+         
+
+
+
+
+          
+          
+          </div>
+
+         
+
+
+      </div>
 
 
       </div>
