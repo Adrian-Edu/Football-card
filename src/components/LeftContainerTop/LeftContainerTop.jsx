13c63419d4ee178cfb8db5ea03be7426552cb03b
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2022-07-08 08:43:02 +0300

    Plm

---
+++ src/components/LeftContainerTop/LeftContainerTop.jsx
@@ -0,0 +1,15 @@
+
+function RightTop () {
+    return (
+        <div className="left-container">
+        <div className="container">
+          <h1>Football Teams</h1>
+          <div className="mid-container">
+            <form className="form">
+              <input></input>
+              <input></input>
+              <button>Add a Team</button>
+            </form>
+          </div>
+    )
+}
